83943c74e80f6f3e9f28062e126f194e0f8c45d2
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2016-10-18 17:02:32 -0400

    Travis deployment test

--- src/routes/health.js
+++ src/routes/health.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 var healthCheckFn = function(req, res){
-    res.send("i am healthy !!!");
+    res.send("i am healthy !!! travis test !!!");
 };
 
 router.get('/', healthCheckFn);
