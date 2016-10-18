7424431ce6ada4409950b542ae374cf7f7443069
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2016-10-18 16:28:14 -0400

    Full deployment cycle from local completed.
      - cloudformation stack is updated based on component change instead of delete and recreate
      - wholde update cycle is managed by having checsum in in cloud deployment resources
      - this saves bandwidth and storage cost for S3 too.
    
      TODO: complete this in travis ci.

--- deploy/scripts/aws/archieve_n_upload.sh
+++ deploy/scripts/aws/archieve_n_upload.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # do the archieval if needed
-echo ATARTTTT
+
 # loop through all the files under dist dir and if the file does not exist - delete the file with similar prefix and upload the new one
 for filepath in dist/*.*
 do
-        echo $filepath
     filename=${filepath##*/}
     AWS_S3_FILE="$("$AWS_CLI" s3 ls s3://"$AWS_LAMDA_BUCKET_NAME"/"$filename")"
-    if [ -z "$STACK_ALIVE" ]; then
+    if [ -z "$AWS_S3_FILE" ]; then
 #       no file with same name found in s3 - file name checksum and content changed in this build
 #       so delete and upload new file
         extension=${filename##*.}
