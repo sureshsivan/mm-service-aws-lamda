36fa7c73da97f8a0375f934cb348c546d8401dfe
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2016-11-18 22:22:20 -0500

    API Gateway Domain test

--- src/routes/health.js
+++ src/routes/health.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 var healthCheckFn = function(req, res){
-    res.send("i am healthy as devil - sarooosdfnvlkisdjfb56476547654vnkdsjfbk!!! ");
+    res.send("i am healthy as devil - domain test with api gateway through travis CI");
 };
 
 router.get('/', healthCheckFn);
