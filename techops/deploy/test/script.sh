34e32fdf0816ae9adc769572b0b3792dd7d6dca6
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2016-11-23 21:28:44 -0500

    poc for AWS cloudfgormation with custom cfn works-

--- techops/deploy/test/script.sh
+++ techops/deploy/test/script.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #export AWS_API_GATEWAY_SECRET=3ric550n
 #http://stackoverflow.com/questions/17117687/simple-openssl-string-decryption-issue
-
+#
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=cat /home/suresh/tmp/3/csr_file | openssl enc -base64 -e -aes-256-cbc -nosalt -pass pass::$AWS_API_GATEWAY_SECRET
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY=cat /home/suresh/tmp/3/private_key | openssl enc -base64 -e -aes-256-cbc -nosalt -pass pass::$AWS_API_GATEWAY_SECRET
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN=cat /home/suresh/tmp/3/sivashub.com/OtherServer/root.crt | openssl enc -base64 -e -aes-256-cbc -nosalt -pass pass::$AWS_API_GATEWAY_SECRET
-
+#
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=openssl enc -base64 -e -aes-256-cbc -nosalt -in /home/suresh/tmp/3/csr_file -pass pass:$AWS_API_GATEWAY_SECRET
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET
-
+#
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY=echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN=echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET
-
-
+#
+#
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=$(cat /home/suresh/tmp/3/csr_file | openssl enc -base64 -e -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET)
 #export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY | openssl enc -base64 -d -aes-256-cbc -nosalt -pass pass:$AWS_API_GATEWAY_SECRET
-
+#
 #var fs = require('fs');
 #exports = module.exports = function(){
 #    function base64_encode(file) {
@@ -43,7 +43,7 @@
 #    console.log("###### BASE 64 ENCODED CERT Chain##########");
 #    console.log(certChain);
 #};
-
+#
 #echo "#######################################"
 #cat /home/suresh/tmp/3/csr_file
 #echo "#######################################"
@@ -51,12 +51,10 @@
 #echo "#######################################"
 #echo $ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY
 #echo "#######################################"
+#
 
-export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY=
-export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN=
-export ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY=
-
-
+#
+#
 # AWS Api Gateway Test
 $AWS_CLI cloudformation create-stack                                                                                \
         --stack-name TEST-API-STACK-WITH-CUSTOM-DOMAIN                                                              \
@@ -69,16 +67,14 @@ $AWS_CLI cloudformation create-stack                                            
             ParameterKey=ParamCertificateChainEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN                     \
             ParameterKey=ParamCertificatePrivateKeyEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY
 
-
-
 # RDS TEst
-$AWS_CLI cloudformation create-stack                                                                                \
-        --stack-name TEST-API-STACK-WITH-CUSTOM-DOMAIN                                                              \
-        --template-body file://./techops/deploy/test/api-custom-domain-test.yaml                                    \
-        --capabilities CAPABILITY_IAM                                                                               \
-        --parameters                                                                                                \
-            ParameterKey=ParamApiDomainName,ParameterValue=test.$AWS_API_G_CUST_DOMAIN_NAME                         \
-            ParameterKey=ParamCertificateName,ParameterValue=TEST$AWS_API_G_CUST_DOMAIN_CERT_BASE_NAME              \
-            ParameterKey=ParamCertificateBodyEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY                       \
-            ParameterKey=ParamCertificateChainEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN                     \
-            ParameterKey=ParamCertificatePrivateKeyEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY
+#$AWS_CLI cloudformation create-stack                                                                                \
+#        --stack-name TEST-API-STACK-WITH-CUSTOM-DOMAIN                                                              \
+#        --template-body file://./techops/deploy/test/api-custom-domain-test.yaml                                    \
+#        --capabilities CAPABILITY_IAM                                                                               \
+#        --parameters                                                                                                \
+#            ParameterKey=ParamApiDomainName,ParameterValue=test.$AWS_API_G_CUST_DOMAIN_NAME                         \
+#            ParameterKey=ParamCertificateName,ParameterValue=TEST$AWS_API_G_CUST_DOMAIN_CERT_BASE_NAME              \
+#            ParameterKey=ParamCertificateBodyEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY                       \
+#            ParameterKey=ParamCertificateChainEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN                     \
+#            ParameterKey=ParamCertificatePrivateKeyEncoded,ParameterValue=$ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY
