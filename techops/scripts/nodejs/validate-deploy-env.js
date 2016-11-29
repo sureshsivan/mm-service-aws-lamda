c5af8f03435083e9a2be0a1e72ad049e3f95b8bf
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2016-11-29 17:49:05 -0500

    Dev stack working fine.

--- techops/scripts/nodejs/validate-deploy-env.js
+++ techops/scripts/nodejs/validate-deploy-env.js
@@ -3,12 +3,6 @@ var checkEnvConfig = function(){
     if(! process.env.AWS_CLI){
         throw new Error("AWS_CLI env var not set");
     }
-    if(! process.env.ROOT_DOMAIN_NAME){
-        throw new Error("ROOT_DOMAIN_NAME env var not set");
-    }
-    if(! process.env.APP_DOMAIN_NAME){
-        throw new Error("APP_DOMAIN_NAME env var not set");
-    }
     if(! process.env.AWS_ACCOUNT_ID){
         throw new Error("AWS_ACCOUNT_ID env var not set");
     }
@@ -18,20 +12,41 @@ var checkEnvConfig = function(){
     if(! process.env.AWS_SECRET_ACCESS_KEY){
         throw new Error("AWS_SECRET_ACCESS_KEY env var not set");
     }
-    if(! process.env.STACK_NAME){
-        throw new Error("STACK_NAME env var not set");
-    }
     if(! process.env.AWS_DEFAULT_REGION){
         throw new Error("AWS_DEFAULT_REGION env var not set");
     }
     if(! process.env.AWS_REGION){
         throw new Error("AWS_REGION env var not set");
     }
+    if(! process.env.APP_FULL_NAME){
+        throw new Error("APP_FULL_NAME env var not set");
+    }
+    if(! process.env.APP_DESCRIPTION){
+        throw new Error("APP_DESCRIPTION env var not set");
+    }
+    if(! process.env.APP_ID_UPPERCASE){
+        throw new Error("APP_ID_UPPERCASE env var not set");
+    }
+    if(! process.env.APP_ID_LOWERCASE){
+        throw new Error("APP_ID_LOWERCASE env var not set");
+    }
+    if(! process.env.ROOT_DOMAIN_NAME){
+        throw new Error("ROOT_DOMAIN_NAME env var not set");
+    }
+    if(! process.env.DEPLOY_ENV){
+        throw new Error("DEPLOY_ENV env var not set");
+    }
+    if(! process.env.ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY){
+        throw new Error("ENCODED_AWS_API_G_CUST_DOMAIN_CERT_BODY env var not set");
+    }
+    if(! process.env.ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY){
+        throw new Error("ENCODED_AWS_API_G_CUST_DOMAIN_CERT_PRIVATE_KEY env var not set");
+    }
+    if(! process.env.ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN){
+        throw new Error("ENCODED_AWS_API_G_CUST_DOMAIN_CERT_CHAIN env var not set");
+    }
     if(! process.env.AWS_LAMDA_BUCKET_NAME){
         throw new Error("AWS_LAMDA_BUCKET_NAME env var not set");
-    }
-    if(! process.env.LAMBDA_FN_BASE_NAME){
-        throw new Error("LAMBDA_FN_BASE_NAME env var not set");
     }
 };
 checkEnvConfig();
